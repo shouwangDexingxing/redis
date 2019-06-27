3ca6db66023e20120633d2ca4a58946971f3de75
showwangDexingxing <zhangzeping_123@163.com>
2019-06-27 18:45:11 +0800

    commig redis

---
+++ redis/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zgj
+ * Date: 2019/6/27
+ * Time: 15:56
+ */
+phpinfo();
+
+new Redis();
+//$redis=new Redis();
+//if($redis){ print_r($redis);    }
+//else{   echo 'noting';  }
